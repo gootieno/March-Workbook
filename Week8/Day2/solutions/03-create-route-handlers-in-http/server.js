7e7520ba55ae30d9aadb36c926c488bd0f0d433e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-05-09 13:33:13 -0700

    create route handlers solution

--- Week8/Day2/solutions/03-create-route-handlers-in-http/server.js
+++ Week8/Day2/solutions/03-create-route-handlers-in-http/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 let nextDogId = 1;
 
@@ -20,11 +20,12 @@ const server = http.createServer((req, res) => {
   req.on("end", () => {
     // Parsing the body of the request
     if (reqBody) {
+      // affiliate=nasa&query=Mars+Rover%21
       req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .split("&") // [affiliate=nasa,query=Mars+Rover%21]
+        .map((keyValuePair) => keyValuePair.split("=")) // [[affiliate,nasa],[query,Mars+Rover%21]]
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")]) // [[affiliate,nasa],[query,Mars Rover%21]]
+        .map(([key, value]) => [key, decodeURIComponent(value)]) // [[affiliate,nasa],[query,Mars Rover!]]
         .reduce((acc, [key, value]) => {
           acc[key] = value;
           return acc;
@@ -34,15 +35,54 @@ const server = http.createServer((req, res) => {
     // Do not edit above this line
 
     // define route handlers here
+    if (req.method === "GET" && req.url === "/") {
+      const responseBody = "Dog Club";
 
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url === "/dogs") {
+      const responseBody = "Dog Index";
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url.startsWith("/dogs")) {
+      console.log("req url ", req.url);
+      const urlParts = req.url.split("/");
+      console.log("url parts ", urlParts);
+
+      if (urlParts.length === 3) {
+        const dogId = urlParts[urlParts.length - 1];
+        if (dogId !== "new") {
+          const responseBody = `Dog details for dogId: ${dogId}`;
+
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(responseBody);
+        }
+      }
+    }
+
+    if (req.method === "POST" && req.url === "/dogs") {
+      const dogId = getNewDogId();
+
+      res.statusCode = 302;
+      res.setHeader("Location", `/dogs/${dogId}`);
+      return res.end();
+    }
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
--- Week8/Day2/solutions/04-serve-static-assets-in-http/server.js
+++ Week8/Day2/solutions/04-serve-static-assets-in-http/server.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+
+  // create a route handler for '/' to respond with the index.html file
+
+  /*
+    1. get the response body
+    2. set status code
+    3. set header
+    4. return body and end res. 
+  */
 });
 
 const port = 5000;

a4042c11de7f8434ced61aaa91d90828e7af68ed
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-05-10 13:18:02 -0700

    fetch from server solutio

--- Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,48 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
+  const url = "/dogs";
+  const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+  // Use the URLSearchParams API to format your body as shown below
+  const body = new URLSearchParams({
+    name: "Kaido",
+    age: 1,
+  });
+
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: body,
+  };
+
+  return fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
+  return fetch("/dogs", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      name,
+      age,
+    }),
+  });
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: { AUTH: "ckyut5wau0000jyv5bsrud90y" },
+  });
 }
--- Week8/Day3/solutions/04-fetch/fetch-requests.js
+++ Week8/Day3/solutions/04-fetch/fetch-requests.js
@@ -7,19 +7,23 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
+const bestSellingProducts = async () => {
+  const response = await fetch("/products/best-selling");
+  console.log("response ", response);
 
+  console.log("response ok ", response.ok);
+  console.log("response text ", await response.text());
+};
+
+bestSellingProducts()
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
 
-
-
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
 
 /* ============== 4. Print the body of the response as text =============== */
 

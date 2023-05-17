a06599790339ae2dad39504751a6efd1f1e9f060
Tanner Shaw <traumaaa@gmail.com>
2023-05-17 11:30:57 -0500

    event listeners prac solutions

--- Week9/Day3/solutions/01-event-listeners/events.js
+++ Week9/Day3/solutions/01-event-listeners/events.js
@@ -1,1 +1,57 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', () => {
+
+
+  // alert('DOMContent is loaded')
+
+
+  const redInput = document.querySelector('#red-input');
+
+  const changeRed = e => {
+    const value = e.currentTarget.value.toLowerCase();
+
+    if (value === 'red') {
+      redInput.style.backgroundColor = 'red'
+    } else {
+      redInput.style.backgroundColor = 'transparent'
+    }
+  }
+
+  redInput.addEventListener('input', changeRed);
+
+
+  const button = document.querySelector('#add-item');
+  const input = document.querySelector('#list-add');
+  const ul = document.querySelector('#section-2 ul');
+
+  const addItem = e => {
+    const value = input.value;
+    console.log(input.value);
+    console.log(input);
+    const li = document.createElement('li');
+    li.innerText = value;
+    ul.appendChild(li);
+    input.value = '';
+  }
+
+  button.addEventListener('click', addItem);
+
+
+  const color = document.querySelector('#color-select');
+  const section = document.querySelector('#section-3');
+
+  const colorChange = e => {
+    section.style.backgroundColor = color.value;
+  }
+
+  color.addEventListener('input', colorChange);
+
+
+  const removeButton = document.querySelector('#remove-listeners');
+
+  removeButton.addEventListener('click', e => {
+    redInput.removeEventListener('input', changeRed);
+    button.removeEventListener('click', addItem);
+    color.removeEventListener('input', colorChange);
+  })
+})
